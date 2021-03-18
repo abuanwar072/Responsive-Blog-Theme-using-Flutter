7f89b9e189cca8ba0bf31f1cbc23adb59a97b8d2
Abu Anwar <abuanwar072@gmail.com>
2021-03-18 14:39:19 +0600

    initial

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF3B1D);
+const kDarkBlackColor = Color(0xFF191919);
+const kBgColor = Color(0xFFE7E7E7);
+const kBodyTextColor = Color(0xFF666666);
+
+const kDefaultPadding = 20.0;
+const kMaxWidth = 1232.0;
+const kDefaultDuration = Duration(milliseconds: 250);
---
+++ lib/models/Blog.dart
@@ -0,0 +1,29 @@
+class Blog {
+  final String date, title, description, image;
+
+  Blog({this.date, this.title, this.description, this.image});
+}
+
+List<Blog> blogPosts = [
+  Blog(
+    date: "23 September 2020",
+    image: "assets/images/0.png",
+    title: "How Ireland’s biggest bank executed a comp lete security redesign",
+    description:
+        "Mobile banking has seen a huge increase since Coronavirus. In fact, CX platform Lightico found that 63 percent of people surveyed said they were more willing to try a new digital banking app than before the pandemic.So while you may be more inclined to bank remotely these days, cybercrime—especially targeting banks—is on the rise.",
+  ),
+  Blog(
+    date: "21 August  2020",
+    image: "assets/images/1.png",
+    title: "5 Examples of Web Motion Design That Really Catch Your Eye",
+    description:
+        "Web animation has become one of the most exciting web design trends in 2020. It breathes more life into a website and makes user interactions even more appealing and intriguing. Animation for websites allows introducing a brand in an exceptionally creative way in modern digital space. It helps create a lasting impression, make a company",
+  ),
+  Blog(
+    date: "23 September 2020",
+    image: "assets/images/2.png",
+    title: "The Principles of Dark UI Design",
+    description:
+        "Mobile banking has seen a huge increase since Coronavirus. In fact, CX platform Lightico found that 63 percent of people surveyed said they were more willing to try a new digital banking app than before the pandemic.So while you may be more inclined to bank remotely these days, cybercrime—especially targeting banks—is on the rise.",
+  ),
+];
